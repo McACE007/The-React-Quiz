62474089ad63b75cf17724f05f3e7bbf5898230f
mcace007 <mrinalace@gmail.com>
2024-01-13 18:32:55 +0530

    Update: Added CSS & assests/reactLogo.svg. Create Header & Main Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
-  return <></>;
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question</p>
+      </Main>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import reactLogo from "../../assets/react.svg";
+
+export default function Header() {
+  return (
+    <header className="app-header">
+      <img src={reactLogo} alt="React Logo" />
+      <h1>The React Quiz</h1>
+    </header>
+  );
+}
